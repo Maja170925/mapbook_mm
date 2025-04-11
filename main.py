3163447f5415b58895eaf113bbe819621e4fc7d7
Maja170925 <mmaja172005@gmail.com>
2025-04-11 09:22:45 +0200

    wip: simple data structure prepater

---
+++ main.py
@@ -0,0 +1,16 @@
+users:list = [
+    {"name":"Maja","location":"Świecie","posts":400},
+    {"name":"Krzysztof","location":"Białobrzegi","posts":500},
+    {"name":"Zuzanna","location":"Radzyń_Podlaski","posts":300},
+    {"name":"Bernard","location":"Ełk","posts":700},
+]
+
+
+
+def get_user_info(users_data:list)->None:
+
+      for user in users_data:
+            print(f"Twój znajomy {user['name']}! z miejscowości {user['location']} opublikował {user['posts']} postów")
+
+
+get_user_info(users)
