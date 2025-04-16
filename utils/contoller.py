3f04332391e8a0d5f10fa66e4703f961b3265fbf
Maja170925 <mmaja172005@gmail.com>
2025-04-16 10:56:39 +0200

    wip: new function: add new user

--- utils/contoller.py
+++ utils/contoller.py
@@ -1,3 +1,9 @@
 def get_user_info(users_data: list) -> None:
     for user in users_data:
         print(f"Twój znajomy {user['name']}! z miejscowości {user['location']} opublikował {user['posts']} postów")
+
+def add_user(users_data: list) -> None:
+    user_name = input("podaj imię nowego użytkownika: ")
+    user_location = input("podaj lokalizacje nowego znajomego: ")
+    user_posts = int(input("podaj liczbę postów: "))
+    users.append({"name": user_name, "location": user_location, "posts": user_posts})
