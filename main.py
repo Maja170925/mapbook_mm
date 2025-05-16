55b9b8cf39bdf2bbb6b5d4a798828f4dfed1a114
Maja170925 <mmaja172005@gmail.com>
2025-05-16 09:26:16 +0200

    wip: tk appgui

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-from utils.model import users
-from utils.contoller import get_user_info, add_users, remove_users, update_user, get_map
+from tkinter import *
 
 
-def main():
-    while True:
 
-        print("===============MENU====================")
-        print("0 - zakończ program")
-        print("1 - pokaż co u znajomych")
-        print("2 - dodaj nowego znajomego")
-        print("3 - usuń znajomego")
-        print("4-aktualizuj dane znajomego")
-        print("5 - utwórz mape znajomego")
-        print('============================================')
-        choice = input ("wybierz opcje menu:")
-        if choice == "0": break
-        if choice == "1": get_user_info(users)
-        if choice == "2": add_users(users)
-        if choice == "3": remove_users(users)
-        if choice == "4": update_user(users)
-        if choice == "5": get_map(users)
+root = Tk()
+root.geometry("1200x760")
+root.title("mapbook_mm")
 
 
-if __name__ == "__main__":
-    main()
+
+root.mainloop()
